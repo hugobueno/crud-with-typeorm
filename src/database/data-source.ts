741aa9da3b6c6d3714480d1b2e019de299976984
Hugo Bueno <hugobueno@live.com>
2022-11-06 16:37:35 -0400

    fix: remove params fixed

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -14,11 +14,11 @@ console.log({
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "crud_test",
+    host: process.env.TYPEORM_HOST,
+    port:  Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
     entities: [
         "src/entities/**/*{.js,.ts}"
      ],
